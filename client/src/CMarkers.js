b019cd7bec4f71b1d86e39116f48260b412f9a93
steveraaen <sraaen@gmail.com>
2017-08-13 12:16:13 -0400

    trying to deploy with sign data

--- client/src/CMarkers.js
+++ client/src/CMarkers.js
@@ -122,7 +122,7 @@ onEachFeature(feature, layer) {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://api.mapbox.com/styles/v1/sraaen/cj52ii4g62aqy2so4s6zbl9g9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3JhYWVuIiwiYSI6ImNqMmt2Y3k4djAwNGczM3IzaWU1a3E1eW8ifQ.dTGNBuW1jqOckGIAEDOUZw"
         />
-{/*        <GeoJSON  key={this.props.keys} data={this.props.data}  children={this.props.text} pointToLayer={this.pointToLayer.bind(this)} onEachFeature={this.onEachFeature.bind(this)} />*/}
+        <GeoJSON  key={this.props.keys} data={this.props.data}  children={this.props.text} pointToLayer={this.pointToLayer.bind(this)} onEachFeature={this.onEachFeature.bind(this)} />
   {/*     <Polygon positions={this.props.latlngs} name={this.props.hoodName} />*/}
        
         
