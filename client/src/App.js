0f855090f6f8ae6226b369ae5729efae8c48317b
Steven J Raaen <stevenjraaen@Stevens-MacBook-Pro.local>
2017-11-08 12:02:57 -0500

    added lockfile

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         }
         this.fetchSigns = this.fetchSigns.bind(this)
         this.setPlaceLoc = this.setPlaceLoc.bind(this)
-       
+
     }
     fetchSigns(placeLoc) {
 
@@ -32,65 +32,65 @@ class App extends Component {
             var keyArr = respo.data.map((k_, idx) => {
                 return 'k_' + idx
             })
-            var test1 =[]
-            for(let i = 0; i < respo.data.length; i++) {
+            var test1 = []
+            for (let i = 0; i < respo.data.length; i++) {
                 var time = respo.data[i].properties.T.split('-')[1]
                 respo.data[i].properties.time = time
-                    test1.push(respo.data[i].properties.time)
-                    }
+                test1.push(respo.data[i].properties.time)
+            }
 
-               const timeSummary = respo.data.reduce((count, code) => {
-                  count[code.properties.time] = (count[code.properties.time] || 0) + 1;
-                  return count;
-                }, {})
-               
+            const timeSummary = respo.data.reduce((count, code) => {
+                count[code.properties.time] = (count[code.properties.time] || 0) + 1;
+                return count;
+            }, {})
 
-                var result = Object.keys(timeSummary).map(function(key, idx) {
-                    var arrSplit = key.split(' ')
-                    var t = moment(arrSplit[0], 'HH:mm A' )
-                    
-                   
-                    return {
-                        y: timeSummary[key],
-                        x: String(key),
-                        tod: t,
-                        days: arrSplit              
-                    };
-                });
-                
-            var timeObjects = []    
-            for(let j = 0; j < result.length; j++) {
 
-                        var geojsonMarker = {
-                            radius: 3,
-                            fillColor: this.state.dotColors[j],
-                            color: "#000",
-                            weight: 1,
-                            opacity: 1,
-                            fillOpacity: 0.8
-                        };
-                        var timeStyle = {
-                            color: this.state.dotColors[j],
-                            text: result[j].x,
-                            count: result[j].y * 5,
-                            time: result[j].tod
-                        }
-                                            
-                for(let i = 0; i < respo.data.length; i++) {
-                    if(respo.data[i].properties.time.includes(result[j].x)) {
-                       respo.data[i].properties.style = geojsonMarker                      
+            var result = Object.keys(timeSummary).map(function(key, idx) {
+                var arrSplit = key.split(' ')
+                var t = moment(arrSplit[0], 'HH:mm A')
+
+
+                return {
+                    y: timeSummary[key],
+                    x: String(key),
+                    tod: t,
+                    days: arrSplit
+                };
+            });
+
+            var timeObjects = []
+            for (let j = 0; j < result.length; j++) {
+
+                var geojsonMarker = {
+                    radius: 3,
+                    fillColor: this.state.dotColors[j],
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                };
+                var timeStyle = {
+                    color: this.state.dotColors[j],
+                    text: result[j].x,
+                    count: result[j].y * 5,
+                    time: result[j].tod
+                }
+
+                for (let i = 0; i < respo.data.length; i++) {
+                    if (respo.data[i].properties.time.includes(result[j].x)) {
+                        respo.data[i].properties.style = geojsonMarker
                     }
                 }
                 timeObjects.push(timeStyle)
             }
-          timeObjects.sort(function(a,b) {return (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0);} );
-            
+            timeObjects.sort(function(a, b) { return (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0); });
+
             this.setState({
                 data: respo.data,
                 keys: keyArr,
                 timeObjects: timeObjects
             })
-          
+
         }.bind(this))
         console.log(this.state)
     }
@@ -113,43 +113,59 @@ class App extends Component {
         this.setState({ placeLoc: place })
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
     }
     render() {
 
-        return (
-      <div className="App">
-      <h2>NYC Street Parking</h2>
-      
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 well">
+        return ( <
+            div className = "App" >
+            <
+            h2 > NYC Street Parking < /h2>
 
-                <PlaceForm sessionLoc={this.state.sessionLoc} data={this.state.data} setPlaceLoc={this.setPlaceLoc} fetchSigns={this.fetchSigns} />             
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12 well">
-              <Stats sessionLoc={this.state.sessionLoc} data={this.state.data} timeObjects={this.state.timeObjects} dotColors={this.state.dotColors}/>  
-              </div>
-          </div>
-          <div className="row">
+            <
+            div className = "container" >
+            <
+            div className = "row" >
+            <
+            div className = "col-sm-12 well" >
 
-            <div className="col-sm-1">
-            </div>
-            <div className="col-sm-10 well">
-             <MapContainer  data={this.state.data} keys={this.state.keys}   sessionLoc={this.state.sessionLoc} placeLoc={this.state.placeLoc} userLoc={this.state.userLoc}/>
-            </div>
-            <div className="col-sm-1">
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12">
-            </div>
-          </div>
-        </div>       
-      </div>
+            <
+            PlaceForm sessionLoc = { this.state.sessionLoc } data = { this.state.data } setPlaceLoc = { this.setPlaceLoc } fetchSigns = { this.fetchSigns }
+            />              <
+            /div> <
+            /div> <
+            div className = "row" >
+            <
+            div className = "col-sm-12 well" >
+            <
+            Stats sessionLoc = { this.state.sessionLoc } data = { this.state.data } timeObjects = { this.state.timeObjects } dotColors = { this.state.dotColors }
+            />   <
+            /div> <
+            /div> <
+            div className = "row" >
+
+            <
+            div className = "col-sm-1" >
+            <
+            /div> <
+            div className = "col-sm-10 well" >
+            <
+            MapContainer data = { this.state.data } keys = { this.state.keys } sessionLoc = { this.state.sessionLoc } placeLoc = { this.state.placeLoc } userLoc = { this.state.userLoc }
+            /> <
+            /div> <
+            div className = "col-sm-1" >
+            <
+            /div> <
+            /div> <
+            div className = "row" >
+            <
+            div className = "col-sm-12" >
+            <
+            /div> <
+            /div> <
+            /div>        <
+            /div>
         );
     }
 }
